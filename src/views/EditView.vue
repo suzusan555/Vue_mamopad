62c37b97956b82c00591d39469d78ada6f3832cc
鈴木智昭 <suzukitomoaki@suzukitomoakinoMacBook-Air.local>
2022-10-13 15:16:04 +0900

    メモアプリ実装

---
+++ src/views/EditView.vue
@@ -0,0 +1,20 @@
+<template>
+    <MemoFrom v-if="memo" :memo="memo" />
+    <p v-else>指定されたメモはありません</p>
+</template>
+
+<script>
+import MemoFrom from '@/components/MemoForm.vue'
+export default {
+    name: 'EditView',
+    components: {
+        MemoFrom,
+    },
+    computed: {
+        memo () {
+            const id = parseInt(this.$route.params.id)
+            return this.$store.getters.getMemoById(id)
+        }
+    }
+}
+</script>
